3b1aef805c671a97ac92aef9a56e2955149ab6ee
João Lacerda <joao.sclf@gmail.com>
2024-06-21 17:24:03 -0300

    add images

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const text: string = "hello world"
-console.log(text)
+interface Command {
+    name: string,
+    description: string,
+    run: (args: string) => any
+}
+
+const command: Command = {
+    name: "teste",
+    description: "teste2",
+    run(args){
+        
+    }
+}
