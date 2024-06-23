b132edd0ceac296623b45c18a84e921a34a41786
João Lacerda <joao.sclf@gmail.com>
2024-06-22 21:58:26 -0300

    add filers

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-interface Command {
-    name: string,
-    description: string,
-    run: (args: string) => any
-}
+import { Message } from "discord.js";
+import { ExtendedCliente } from "./structs/ExtendClient"
+export * from "colors";
 
-const command: Command = {
-    name: "teste",
-    description: "teste2",
-    run(args){
-        
-    }
-}
+const Client = new ExtendedCliente();
+
+Client.start();
+
+export { Client }
+
+Client.on("ready", () => {
+    console.log("Bot online!". green)
+})
+
+Client.on("messageCreate", (Message) => {
+    if (Message.author.id == Client.user?.id) return;
+
+
+    Message.reply({
+        content: `Olá, ${Message.author.displayName} meu pai é o @_thurow, também conhecido como pai da indanidade!`
+    })
+})
